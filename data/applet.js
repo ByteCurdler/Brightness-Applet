8ee15ecb3efdb4d1a04e7bab92d47b7c9fb27d92
Python Whiz 256 <mostwonderfulboy@gmail.com>
2019-11-24 13:27:12 -0700

    Fix constants not doing anything

--- data/applet.js
+++ data/applet.js
@@ -2,7 +2,6 @@
 //   force-quit@cinnamon.org for basic applet
 //   redshift@marvel4u for popup menu
 
-
 const Lang = imports.lang;
 const Applet = imports.ui.applet;
 const GLib = imports.gi.GLib;
@@ -12,8 +11,10 @@ const UUID = "brightness@ILikePython256";
 
 const APPLET_DIR = GLib.get_home_dir() + "/.local/share/cinnamon/applets/" + UUID + "/";
 
-const BRIGHT_MIN = 0.5;
-const BRIGHT_MAX = 1.5;
+const BRIGHT_MIN = 50;
+const BRIGHT_MAX = 150;
+const BRIGHT_STEP = 5;
+const BRIGHT_START = 100;
 
 Gettext.bindtextdomain(UUID, GLib.get_home_dir() + "/.local/share/locale");
 
@@ -45,13 +46,14 @@ MyApplet.prototype = {
 
             this.brightnessMenuItem = new PopupMenu.PopupMenuItem("", { reactive: false });
             this.menu.addMenuItem(this.brightnessMenuItem);
-
+			
+			let sliderPos = (BRIGHT_START - BRIGHT_MIN) / (BRIGHT_MAX - BRIGHT_MIN)
             //brightness slider control
-            this._brightnessSlider = new PopupMenu.PopupSliderMenuItem(0.5);
+            this._brightnessSlider = new PopupMenu.PopupSliderMenuItem(sliderPos);
             this._brightnessSlider.connect("value-changed", Lang.bind(this, this.brightnessSliderChanged));
             this.menu.addMenuItem(this._brightnessSlider);
             
-			this.brightnessSliderChanged(this._brightnessSlider, 0.5);
+			this.brightnessSliderChanged(this._brightnessSlider, sliderPos);
         }
         catch (e) {
             global.logError(e);
@@ -60,15 +62,17 @@ MyApplet.prototype = {
 
     brightnessSliderChanged: function (slider, value) {
         let brightness = parseFloat(value);
-        brightness = Math.round(brightness * 100 + 50);
+        brightness = Math.round(
+        	brightness * (BRIGHT_MAX - BRIGHT_MIN) / BRIGHT_STEP
+        ) * BRIGHT_STEP + BRIGHT_MIN;
         
-        this.brightness = brightness;
-		
-        let brightnessStr = _("Brightness") + ": " + this.brightness + "%";
-        this.brightnessMenuItem.label.text = brightnessStr;
-        if (brightness % 3 == 0) {
-        	GLib.spawn_command_line_async("bash " + APPLET_DIR + "brightness.bash " + (brightness / 100));
-        }
+        if (this.brightness != brightness) {
+		    this.brightness = brightness;
+			
+		    let brightnessStr = _("Brightness") + ": " + this.brightness + "%";
+		    this.brightnessMenuItem.label.text = brightnessStr;
+			GLib.spawn_command_line_sync("bash " + APPLET_DIR + "brightness.bash " + (brightness / 100));
+		}
     },
 
     on_applet_clicked: function (event) {
